4dfe990d3497c371c5c00ff566ecec54d7e012c3
nic <nic@tnsbuilds01.tn.ixsystems.net>
2022-06-27 10:35:49 +0100

    Update args
    Update cmd line args

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM alpine:latest
 
-RUN apk add --no-cache ipmitool
+RUN apk add --no-cache ipmitool jo mosquitto-clients
 
+ADD ipmi2mqtt.sh .
 
-CMD ["ipmitool", "sensor"]
+CMD ["./ipmi2mqtt.sh"]
--- ipmi2mqtt.sh
+++ ipmi2mqtt.sh
@@ -1,8 +1,17 @@
 #!/bin/sh 
 set +eu
 
+IPMI_HOST_IP="$1"
+SLEEP_PERIOD="$2"
+
 # ipmitool sensor | awk -F'|' '$6 ~ /[0-9.]+/ {print $1 $6}'
 # ipmitool sensor | awk -F'|' '$1~!/.*na.*/ {print $1 $2}'
 
-# Time all white space
-JSON_STRING=$(ipmitool sensor | awk -F'|' '$2~!/.+na.+/ {gsub(/ /, "", $0); print $1 "=" $2}' | jo)
+while true; do
+	# Time all white space
+	JSON_STRING=$(ipmitool sensor | awk -F'|' '$2~!/.+na.+/ {gsub(/ /, "", $0); print $1 "=" $2}' | jo)
+	echo ${JSON_STRING}
+	mosquitto_pub -h "${IPMI_HOST_IP} -t "ipmi/state" -m "${JSON_STRING}"
+	sleep $SLEEP_PERIOD
+done
+exit $?
